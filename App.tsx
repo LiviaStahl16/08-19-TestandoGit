a06a77ae91a20279e8f59db25f3b7e3c78b82c1a
LiviaStahl <livia.stahl@estudante.ifms.edu.br>
2021-08-26 18:32:27 -0400

    finalmente pronto

--- App.tsx
+++ App.tsx
@@ -3,65 +3,62 @@ import React from 'react';
 import { StyleSheet, Text, View, Image, TextInput } from 'react-native';
 
 export default function App() {
-	return (
-		<View style={styles.container}>
-			<View style={styles.header}>
-				<Image source={
-					require("./src/assets/arrow-left.png")
-				}>
-				</Image>
-				<View style={styles.inputTextView}>
-					<Image source={
-						require("./src/assets/ei_search.png")
-					}>
-					</Image>
-					<TextInput
-						placeholder="Search"
-						placeholderTextColor="#F5FFFF"
-						style={styles.textInput}
-					>
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Image source={
+                    require("./src/assets/arrow-left.png")
+                    }>
+                </Image>
+                <View style={styles.inputTextView}>
+                    <Image source={
+                        require("./src/assets/ei_search.png")
+                        }>
+                    </Image>
+                    <TextInput
+                        placeholder="Search"
+                        placeholderTextColor="#F5FFFF"
+                        style={styles.textInput}
+                        >
 
-					</TextInput>
-				</View>
-				<Image source={
-					require("./src/assets/share.png")
-				}>
-				</Image>
-
-
-
-
-
-			</View>
-			<Text>oi :)</Text>
-			<StatusBar style="auto" />
-		</View>
-	);
+                    </TextInput>
+                </View>
+                <Image source={
+                    require("./src/assets/share.png")
+                    }>
+                </Image>
+                
+            </View>
+            <Text>oi :) aaaa</Text>
+            <StatusBar style="auto"  />
+        </View>
+    );
 }
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-	},
-	header: {
-		minHeight: 93,
-		backgroundColor: "#4369B0",
-		paddingTop: 40,
-		flexDirection: "row",
-		justifyContent: "space-between",
-		paddingHorizontal: 30,
-	},
-	inputTextView: {
-		flex: 1,
-		backgroundColor: "#2C4877",
-		height: 40,
-		marginHorizontal: 10,
-		alignItems: "center",
-		borderRadius: 28,
-		flexDirection: "row"
-	},
-	textInput: {
-		color: "#F5FFFF",
-		flex: 1,
-		paddingRight: 10,
-	},
+    container: {
+        flex:1,
+    },
+    header:{
+        minHeight:93,
+        backgroundColor: "#4369B0",
+        paddingTop:40,
+        flexDirection:"row",
+        justifyContent:"space-between",
+        paddingHorizontal:30,
+    },
+    inputTextView:{
+        flex:1,
+        backgroundColor:"#2C4877",
+        height:40,
+        marginHorizontal:10,
+        alignItems: "center",
+        borderRadius:28,
+        flexDirection:"row"
+    },
+    textInput:{
+        color:"#F5FFFF",
+        // maxWidth:200
+        flex:1,
+        paddingRight:10,
+    },
 });
