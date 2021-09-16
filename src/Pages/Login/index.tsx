58762d6665f538a3e1177869353a1cf2980025ab
LiviaStahl <livia.stahl@estudante.ifms.edu.br>
2021-09-16 09:46:35 -0400

    Tarefa Commit da Tela de Login

--- src/Pages/Login/index.tsx
+++ src/Pages/Login/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, View, Text, Image, TextInput, TouchableOpacity, Linking } from 'react-native';
+import {StyleSheet, View, Text, Image, TextInput, TouchableOpacity, Linking } from 'react-native';
 
 const Login = () => {
     return (
